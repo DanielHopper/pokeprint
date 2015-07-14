3a52ef522f0f65cc0bc1212356039d3b0ba8e327
DanielHopper <djhopper95@gmail.com>
2015-07-13 23:25:19 -0700

    basic commander argv parsing, API call based off first arg, added stat display

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
 #! /usr/bin/env node
 
-var poke = require('./pokelist');
+var _ = require('underscore');
+var colors = require('colors');
 var program = require('commander');
 var request = require('request');
+var poke = require('./pokelist');
 
-var defaultOptions = {'name': true, 'stats': true, 'abilities': true};
+var defaultOptions = {'stats': true, 'abilities': true};
+
+
+
 
 // ASCII taken from http://www.asciiartfarts.com/pokemon.html
 
@@ -25,16 +30,56 @@ console.log(
 '               Gotta try {...} catch () {...} em all!         \n');
 
 
+var pokeStats = function (body) {
+
+	var stats = ['hp', 'attack', 'defense', 'sp_atk', 'sp_def', 'speed'];
+	var pkmn = JSON.parse(body);
+
+
+	for (var i in stats) {
+		if (pkmn.hasOwnProperty(stats[i])) {
+			var stat = Math.floor(pkmn[stats[i]]/2);
+				full = '',
+				left = '';
+			for (var x = 0; x <= 100; x++){
+				if (stat) {
+					full+='_';
+					stat--;
+				} else {
+					left+='|'
+				}
+			}
+			console.log(stats[i] + ": " + pkmn[stats[i]]);
+			console.log(full.bgRed + left.bgWhite);
+		}
+	}
+};
+
+var pokeAbilities = function (body) {
+
+	// ability printing
+
+};
+
+var pokeBreeding = function (body) {
+
+	// breeding info printing
+
+};
+
 var pokePrint = function (body, options) {
 
 	var pkmn = JSON.parse(body);
 	options = options || defaultOptions;
 
-	options.name && console.log(pkmn.name);
+	console.log(pkmn.name);
+	options.stats && pokeStats(body);
+	options.abilities && pokeAbilities(body);
+	options.breeding && pokeBreeding(body);
 
 	// etc...
 
-}
+};
 
 
 var getPokemon = function (dexNumber, options) {
@@ -48,9 +93,22 @@ var getPokemon = function (dexNumber, options) {
 		}
 	});
 
-}
+};
+
+var options = {};
+
+program
+	.version('1.0.0')
+	.usage("[options] <pokemon>")
+	.option("-s, --stats", "Display stats of specified pokemon")
+	.option("-a, --abilities", "Display abilities of specified pokemon")
+	.option("-b, --breeding", "Display information about specified pokemon relevant to breeding")
+	.parse(process.argv);
+
+if (program.stats) options = _.extend(options, {'stats': true});
+if (program.abilities) options = _.extend(options, {'abilities': true});
+if (program.breeding) options = _.extend(options, {'breeding': true});
 
 
-// Basic Bulbasaur test
+getPokemon(poke.dex.indexOf(program.args[0]) + 1, options);
 
-getPokemon(poke.dex.indexOf('Bulbasaur') + 1);
